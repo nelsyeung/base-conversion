f38e6288f6210e782b19256060c4a73cd0d62a97
Nelson Yeung <nelsyeung@icloud.com>
2015-02-07 11:32:41 +0000

    Add namespace

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Cpp function to convert numbers from one base representation to another base. Ba
 
 # Usage
 ```cpp
-convert_base(string input, int baseFrom, int baseTo);
-convert_base("100", 2, 10);
+n::convert_base(string input, int baseFrom, int baseTo);
+n::convert_base("100", 2, 10);
 returns "4"
 ```
 
@@ -13,8 +13,9 @@ returns "4"
 #include <iostream>
 #include "base_converter.hpp"
 
-int main(int argc, char const* argv[]) {
-    std::cout << convert_base("100", 2, 10) << std::endl;
+int main()
+{
+    std::cout << n::convert_base("100", 2, 10) << std::endl;
 
     return 0;
 }
--- base_converter.hpp
+++ base_converter.hpp
@@ -3,49 +3,51 @@
 
 #include <string>
 
-std::string convert_base(const std::string &inputStr, int baseFrom, int baseTo) {
-    const std::string alphanum = "0123456789ABCDEFGHIJKLMOPQRSTUVWXYZ";
-    unsigned long inputNum = 0;
-    std::string output = "";
+namespace n {
+    std::string convert_base(const std::string &inputStr, int baseFrom, int baseTo) {
+        const std::string alphanum = "0123456789ABCDEFGHIJKLMOPQRSTUVWXYZ";
+        unsigned long inputNum = 0;
+        std::string output = "";
 
-    // No conversion is needed for the same base
-    if (baseFrom == baseTo) {
-        return inputStr;
-    }
+        // No conversion is needed for the same base
+        if (baseFrom == baseTo) {
+            return inputStr;
+        }
 
-    // Make sure the base range from 2 to 36
-    if (baseFrom < 2) {
-        baseFrom = 2;
-    }
-    else if (baseFrom > 36) {
-        baseFrom = 36;
-    }
+        // Make sure the base range from 2 to 36
+        if (baseFrom < 2) {
+            baseFrom = 2;
+        }
+        else if (baseFrom > 36) {
+            baseFrom = 36;
+        }
 
-    if (baseTo < 2) {
-        baseTo = 2;
-    }
-    else if (baseTo > 36) {
-        baseTo = 36;
-    }
+        if (baseTo < 2) {
+            baseTo = 2;
+        }
+        else if (baseTo > 36) {
+            baseTo = 36;
+        }
 
-    // Convert input to base 10
-    for (std::string::size_type pos = 0; pos < inputStr.length(); ++pos) {
-        inputNum = inputNum * baseFrom + alphanum.find(inputStr[pos]);
-    }
+        // Convert input to base 10
+        for (std::string::size_type pos = 0; pos < inputStr.length(); ++pos) {
+            inputNum = inputNum * baseFrom + alphanum.find(inputStr[pos]);
+        }
 
-    // No conversion is needed when input is 0
-    if (inputNum == 0) {
-        return "0";
-    }
+        // No conversion is needed when input is 0
+        if (inputNum == 0) {
+            return "0";
+        }
 
-    // Convert base 10 number to a new base
-    while (inputNum > 0) {
-        std::string c(1, alphanum[inputNum % baseTo]);
-        output.insert(0, c);
-        inputNum /= baseTo;
-    }
+        // Convert base 10 number to a new base
+        while (inputNum > 0) {
+            std::string c(1, alphanum[inputNum % baseTo]);
+            output.insert(0, c);
+            inputNum /= baseTo;
+        }
 
-    return output;
-}
+        return output;
+    }
+} // namespace n
 
 #endif
