405e8706dbb096b241644c4ac4eb11c0cce56f4f
Nelson Yeung <nelsyeung@Nelsons-MacBook-Pro.local>
2015-02-07 11:31:34 +0000

    Add namespace

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "base_converter.hpp"
 
-int main(int argc, char const* argv[]) {
-    std::cout << convert_base("100", 2, 10) << std::endl;
+int main()
+{
+    std::cout << n::convert_base("100", 2, 10) << std::endl;
 
     return 0;
 }
